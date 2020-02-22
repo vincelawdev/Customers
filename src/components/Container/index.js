42102bdc5b8e77b4a33442c4639d5b281e373b66
Vince Law <vince@siriusdesignlab.com.au>
2020-02-22 16:42:33 +1100

    Add global CSS styles on <body>. Update Container component to display form.

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,9 +1,24 @@
 import styled from 'styled-components';
 
 const Container = styled.main`
-  margin: 0 0 60px 0;
-  padding: 0 30px;
+  width: 80%;
+  margin: 50px auto;
+  padding: 30px;
   display: block;
+  background-color: white;
+
+  h1, h2 {
+    margin: 0 0 30px 0;
+    padding: 0;
+    text-align: center;
+  }
+
+  button {
+    margin: 0 auto;
+    padding: 10px;
+    display: block;
+    font-size: 32px;
+  }
 `;
 
 export default Container;
--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,29 +1,16 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { getCompanyData, getEmployeesData } from './actions';
-import Header from '../../components/Header';
+import React from 'react';
+import GlobalStyle from './global-style';
 import Container from '../../components/Container';
-import Employees from '../../components/Employees';
-import EmployeeLightbox from '../../components/EmployeeLightbox';
 
-const App = () => {
-  const dispatch = useDispatch();
-
-  // get company and employees data
-  useEffect(() => {
-    dispatch(getCompanyData());
-    dispatch(getEmployeesData());
-  }, [dispatch]);
-
-  return (
-    <>
-      <Header />
-      <Container>
-        <Employees />
-      </Container>
-      <EmployeeLightbox />
-    </>
-  );
-};
+const App = () => (
+  <>
+    <GlobalStyle />
+    <Container>
+      <h1>Welcome to the Customers Search Form</h1>
+      <h2>Please click the start button to begin!</h2>
+      <button type="button">Start</button>
+    </Container>
+  </>
+);
 
 export default App;
